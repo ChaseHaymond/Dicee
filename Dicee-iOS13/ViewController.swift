adafe651e3fd8b5c1a91fe50c58dc22db9774b2b
ChaseHaymond <cdhaymond@gmail.com>
2022-07-06 22:15:01 -0600

    Changed the image array to be a Let, and changed how we randomly get an image.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,19 +13,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
     
-    var diceImages = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-    
+    let diceImages = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         // #ImageLiteral
         
-        leftDiceNumber = Int.random(in: 0...5)
-        rightDiceNumber = Int.random(in: 0...5)
-        
-        diceImageViewOne.image = diceImages[leftDiceNumber]
-        diceImageViewTwo.image = diceImages[rightDiceNumber]
+        diceImageViewOne.image = diceImages.randomElement()
+        diceImageViewTwo.image = diceImages.randomElement()
         
     }
 }
